46a308d6ffeeac29e0d4e75738eb41ee3ca3a36c
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-02-13 14:52:23 +0900

    vue-meta

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { createSSRApp } from "vue";
 import { routeMatcher } from "./router/main";
+import { renderToStringWithMeta, createMetaManager, defaultConfig, deepestResolver, useMeta } from "vue-meta";
+import App from "./EmptyComponent"
 
 const resolved = routeMatcher.resolve(
   { path: "/" },
   { matched: [], path: "/", params: {}, name: null, meta: {} },
 );
 
+const metaInfo = resolved.meta.metaInfo
+
 console.log(resolved);
+
+const metaManager = createMetaManager(defaultConfig, deepestResolver.resolve)
+const app = createSSRApp(App)
+app.use(metaManager)
+useMeta(metaInfo ?? {}, metaManager)
+renderToStringWithMeta(app).then(([rendered, context]) => {
+  console.log("renderToStringWithMeta")
+  console.log("teleports", context.teleports)
+})
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,23 @@
 import { RouteRecordRaw } from "vue-router";
+import { MetaSource } from "vue-meta";
+
+declare module "vue-router" {
+  export interface RouteMeta {
+    metaInfo?: MetaSource
+  }
+}
 
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
     component: () => import("../pages/index.vue"),
+    meta: {
+      metaInfo: {
+        htmlAttrs: { lang: "ja" },
+        // meta: [
+        //   { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+        // ],
+      },
+    },
   }
 ]
